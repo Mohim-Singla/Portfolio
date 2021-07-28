0ffb92827ce878e6815b189adb38d2fd4f24536d
Mohim-Singla <mohimsingla@gmail.com>
2021-07-28 13:37:43 +0530

    Changes in the UI

--- JS File/index.js
+++ JS File/index.js
@@ -1,3 +1,13 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.documentElement.scrollTop > 500) {
+    document.getElementById("navbar").className = "test";
+  } else {
+    document.getElementById("navbar").className = "";
+  }
+}
+
 function openit()
 {
     document.getElementById("menu").style.right="0";
